c330ab13a03b99558e64ec61a818e191a44a5c4d
hotzsauce <githotz@gmail.com>
2022-06-15 17:19:43 -0500

    tabs -> four spaces

--- src/types.jl
+++ src/types.jl
@@ -1,33 +1,33 @@
 abstract type SimsOptimResults end
 
 struct OptimizationResults{Tx, Tf, Tg} <: SimsOptimResults
-	method::SimsOptimMethod
-	# 
-	iterations::Int
-	converged::Bool
-	#
-	value::Real
-	minimum::Tf
-	#
-	minimizer::Tx
-	initial_x::Tx
-	#
-	x_converged::Bool
-	x_change::Tx
-	#
-	f_converged::Bool
-	f_change::Tf
-	#
-	g_converged::Bool
-	g_size::Tg
-	#
-	H::Union{AbstractMatrix, Nothing}
-	#
-	f_calls::Int
-	g_calls::Int
-	#
-	flag::Int
-	message::AbstractString
+    method::SimsOptimMethod
+    # 
+    iterations::Int
+    converged::Bool
+    #
+    value::Real
+    minimum::Tf
+    #
+    minimizer::Tx
+    initial_x::Tx
+    #
+    x_converged::Bool
+    x_change::Tx
+    #
+    f_converged::Bool
+    f_change::Tf
+    #
+    g_converged::Bool
+    g_size::Tg
+    #
+    H::Union{AbstractMatrix, Nothing}
+    #
+    f_calls::Int
+    g_calls::Int
+    #
+    flag::Int
+    message::AbstractString
 end
 
 
@@ -65,13 +65,13 @@ f_obj(f::T) where {T<:Real} = abs(f)
 g_obj(g::AbstractArray{T}) where {T<:Real} = maximum(abs, vec(g))
 
 function x_converged(x::AbstractVector{T}, y::AbstractVector{T}, x_tol::T) where {T<:Real}
-	return x_obj(x, y) < x_tol
+    return x_obj(x, y) < x_tol
 end
 x_converged(x::AbstractVector{T}, x_tol::T) where {T<:Real} = x_obj(x) < x_tol
 
 
 function f_converged(f::AbstractVector{T}, g::AbstractVector{T}, f_tol::T) where {T<:Real}
-	return f_obj(f, g) < f_tol
+    return f_obj(f, g) < f_tol
 end
 f_converged(f::AbstractVector{T}, f_tol::T) where {T<:Real} = f_obj(f) < f_tol
 f_converged(f::T, g::T, f_tol::T) where {T<:Real} = f_obj(f, g) < f_tol
@@ -84,36 +84,36 @@ g_converged(g::AbstractArray{T}, g_tol::T) where {T<:Real} = g_obj(g) < g_tol
 
 function Base.show(io::IO, rs::SimsOptimResults)
 
-	converged(rs) ? (status_string = "success") : "failure"
-	println("status using $(method(rs)): $status_string")
+    converged(rs) ? (status_string = "success") : "failure"
+    println("status using $(method(rs)): $status_string")
 
-	println("\nconvergence")
-	println("----------------")
-	println("    x: $(x_converged(rs))")
-	println("    f: $(f_converged(rs))")
-	println("    g: $(g_converged(rs))")
+    println("\nconvergence")
+    println("----------------")
+    println("    x: $(x_converged(rs))")
+    println("    f: $(f_converged(rs))")
+    println("    g: $(g_converged(rs))")
 
-	println("\nobjectives")
-	println("----------------")
-	println("    value    : $(value(rs))")
-	println("    minimum  : $(minimum(rs))")
-	println("    minimizer: $(minimizer(rs))")
+    println("\nobjectives")
+    println("----------------")
+    println("    value    : $(value(rs))")
+    println("    minimum  : $(minimum(rs))")
+    println("    minimizer: $(minimizer(rs))")
 
-	println("\ncounts")
-	println("----------------")
-	println("    total iterations: $(iterations(rs))")
-	println("    f calls         : $(f_calls(rs))")
-	println("    g calls         : $(g_calls(rs))")
+    println("\ncounts")
+    println("----------------")
+    println("    total iterations: $(iterations(rs))")
+    println("    f calls         : $(f_calls(rs))")
+    println("    g calls         : $(g_calls(rs))")
 
-	println("\nimprovements")
-	println("----------------")
-	println("    initial_x: $(initial_x(rs))")
-	println("    x_change : $(x_change(rs))")
-	println("    f_change : $(f_change(rs))")
-	println("    g_size   : $(g_size(rs))")
+    println("\nimprovements")
+    println("----------------")
+    println("    initial_x: $(initial_x(rs))")
+    println("    x_change : $(x_change(rs))")
+    println("    f_change : $(f_change(rs))")
+    println("    g_size   : $(g_size(rs))")
 
-	println("\ninformation")
-	println("----------------")
-	println("    flag   : $(flag(rs))")
-	println("    message: $(message(rs))")
+    println("\ninformation")
+    println("----------------")
+    println("    flag   : $(flag(rs))")
+    println("    message: $(message(rs))")
 end
