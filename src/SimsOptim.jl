e1cb4d301a500c51908fdd48e480a5077417cb83
hotzsauce <githotz@gmail.com>
2023-12-24 20:43:51 -0600

    add precompilation stuff and bump version

--- src/SimsOptim.jl
+++ src/SimsOptim.jl
@@ -2,6 +2,7 @@ module SimsOptim
 
 using ForwardDiff
 using LinearAlgebra
+using PrecompileTools
 using Random
 
 using Formatting: printfmtln
@@ -22,6 +23,16 @@ struct Csminwel <: SimsOptimMethod end
 
 include("types.jl")
 include("algos.jl")
+
+@setup_workload begin
+    cs = Csminwel()
+    x0 = [5.4, 9.2]
+    rosen(x) = 100*(x[2] - x[1])^2 + (1 - x[1])^2
+
+    @compile_workload begin
+        res = optimize(rosen, x0, cs; iterations = 400)
+    end
+end
 
 
 end # module
